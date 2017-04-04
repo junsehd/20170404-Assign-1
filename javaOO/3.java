c79ac30f80e0136f91c89d821737f46ff2280c6e
sjh2428 <pok_gare@naver.com>
2017-04-04 14:02:02 +0900

    3번 문제
    3번 문제

---
+++ javaOO/3.java
@@ -0,0 +1,32 @@
+class Circle {
+	public double radius;
+	public static double PI = 3.141592;
+	
+	//생성자 구현
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+	//현재 반지름을 사용하여 원의 면적을 구하는 메소드
+	public double getArea() {
+		return radius * radius * PI;
+	}
+}
+
+public class Cylinder {
+	Circle cir;
+	double height;
+	
+	public double getVolume() {
+		return cir.radius * cir.radius * cir.PI * height;
+	}
+	
+	public static void main(String[] args) {
+		Cylinder a1 = new Cylinder();
+		
+		a1.cir = new Circle(2.8);
+		a1.height = 5.6;
+		System.out.println("원통의 부피 : " + (a1.getVolume() * a1.height));
+
+	}
+
+}
