ed05c51ba611dd932d7859c3a474ffce11ccb7ee
sjh2428 <pok_gare@naver.com>
2017-04-04 14:12:21 +0900

    4번 문제
    4번 문제

--- javaOO/4.java
+++ javaOO/4.java
@@ -28,9 +28,9 @@ public class Cylinder {
 	}
 	
 	public static void main(String[] args) {
-		Cylinder a1 = new Cylinder(new Circle(2.8), 5.6);
+		Cylinder cd= new Cylinder(new Circle(2.8), 5.6);
 		
-		System.out.println("원통의 부피 : " + a1.getVolume());
+		System.out.println("원통의 부피 : " + cd.getVolume());
 
 	}
 
