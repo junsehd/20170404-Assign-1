6b7b5b28c53b9961905983f7eb46c7a435588b83
sjh2428 <pok_gare@naver.com>
2017-04-04 14:18:37 +0900

    5번 문제
    5번 문제

---
+++ javaOO/5.java
@@ -0,0 +1,19 @@
+public class SalaryMan {
+	int salary = 1000000;
+	
+	public SalaryMan() {}
+	public SalaryMan(int salary) {
+		this.salary = salary;
+	}
+	
+	public int getAnnualGross() {
+		return (salary * 12) + (salary * 5);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(new SalaryMan().getAnnualGross());
+		System.out.println(new SalaryMan(2_000_000).getAnnualGross());
+
+	}
+
+}
